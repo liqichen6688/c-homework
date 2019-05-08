5d2996bbc5e385c7d453e01b22e23c653838f4bc
liqichen <liqichen@me.com>
2019-05-08 09:28:44 +0800

    10

---
+++ Chapter10/Order/Data/Data.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Order.Data
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Client { get; set; }
+    }
+}
---
+++ Chapter10/Order/Data/OrdersContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.Data
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext(DbContextOptions<OrdersContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
